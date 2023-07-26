91e00da6cf728c5f7f98dfec6d69e8c81767cc42
Hayashi Takui <hayashi.azi1mov@outlook.com>
2023-07-26 21:05:17 +0700

    Solved 54: maximum depth of binary tree

---
+++ 54_maximum_depth_of_binary_tree/sol.ex
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+defmodule TreeNode do
+  @type t :: %__MODULE__{
+          val: integer,
+          left: TreeNode.t() | nil,
+          right: TreeNode.t() | nil
+        }
+  defstruct val: 0, left: nil, right: nil
+end
+
+defmodule Helper do
+  def gentree(lst), do: do_gentree({List.to_tuple(lst), length(lst)})
+  defp do_gentree(const, index \\ 0)
+  defp do_gentree({_, size}, index) when index >= size, do: nil
+  defp do_gentree({lst, _} = const, index) do
+    if elem(lst, index) == nil, do: nil,
+    else: %TreeNode{
+        val: elem(lst, index),
+        left: do_gentree(const, 2*index+1), right: do_gentree(const, 2*index+2)
+      }
+  end
+
+  def detree(tree), do: do_detree(tree) |> List.flatten
+    |> Enum.sort_by(& elem(&1, 1), :desc)
+    |> Stream.drop_while(& elem(&1, 0) == nil)
+    |> Stream.map(& elem(&1, 0))
+    |> Enum.reverse
+  defp do_detree(tree, offset \\ 0)
+  defp do_detree(nil, offset), do: {nil, offset}
+  defp do_detree(%TreeNode{val: v, left: l, right: r}, offset) do
+    [{v, offset}, do_detree(l, 2*offset+1), do_detree(r, 2*offset+2)]
+  end
+end
+
+defmodule Solution do
+  @spec max_depth(root :: TreeNode.t | nil) :: integer
+  def max_depth(nil), do: 0
+  def max_depth(%TreeNode{val: v, left: l, right: r}) do
+    1 + max(max_depth(l), max_depth(r))
+  end
+end
+
+tests = [
+  %{input: [3,9,20,nil,nil,15,7], expect: 3},
+  %{input: [1,nil,2], expect: 2},
+  %{input: [], expect: 0},
+  %{input: [1], expect: 1},
+]
+
+for %{input: root, expect: expect} <- tests do
+  Solution.max_depth(Helper.gentree(root))
+  # |> Helper.detree
+  |> (&[result: &1, expect: expect, correct?: &1==expect]).()
+end
+|> dbg
+# |> dbg(limit: :infinity)
